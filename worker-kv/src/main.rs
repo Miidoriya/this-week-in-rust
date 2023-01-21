08a7742b609a6ec5f939c5edfac46d4159bf73e1
Miidoriya <99621562+Miidoriya@users.noreply.github.com>
2023-01-21 16:35:53 +0000

    [create-a-rust-worker] Add reply function to worker-kv

--- worker-kv/src/main.rs
+++ worker-kv/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use wasm_workers_rs::{
+    worker,
+    http::{self, Request, Response},
+    Content, Cache,
+};
+
+#[worker(cache)]
+fn handler(_req: Request<String>, cache: &mut Cache) -> Result<Response<Content>> {
+    Ok(http::Response::builder()
+        .status(200)
+        .header("x-generated-by", "wasm-workers-server")
+        .body(String::from("Hello wasm!").into())?)
 }
